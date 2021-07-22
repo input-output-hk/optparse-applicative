4e6522cc6dc37b597748697e05b0a745bab382de
John Ky <john.ky@iohk.io>
2021-07-22 20:03:00 +1000

    Remove superfluous whitespace

--- src/Options/Applicative/Help/Core.hs
+++ src/Options/Applicative/Help/Core.hs
@@ -166,7 +166,7 @@ foldTree prefs s (MultNode xs) =
                 . fmap (uncurry (<>))
                 . zip leads
                 $ fmap (wrapOver NoDefault MaybeRequired . first (fmap (nest 2)) . foldTree prefs s) xs
-                ) <> pure line
+                )
           )
         compact :: Chunk Doc
         compact =
@@ -178,7 +178,7 @@ foldTree prefs s (MultNode xs) =
     mult_wrap [_] = NeverRequired
     mult_wrap _ = MaybeRequired
     leads :: [Chunk Doc]
-    leads = fmap pure (pretty " ":repeat (line <> pretty "  "))
+    leads = fmap pure (mempty:repeat (line <> pretty "  "))
 
 foldTree prefs s (AltNode b xs) = first (annTrace 3 "foldTree2") $
   (\x -> (x, NeverRequired))
