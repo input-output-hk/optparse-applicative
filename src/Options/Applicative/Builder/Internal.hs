12a08ad3b2f41caef851273877af682f14cf8316
Jordan Millar <9418537+Jimbo4350@users.noreply.github.com>
2026-06-18 16:00:24 -0400

    Remove propGroup from baseProps

--- src/Options/Applicative/Builder/Internal.hs
+++ src/Options/Applicative/Builder/Internal.hs
@@ -151,7 +151,6 @@ baseProps = OptProperties
   , propShowDefault = Nothing
   , propDescMod = Nothing
   , propShowGlobal = True
-  , propGroup = OptGroup []
   }
 
 mkCommand :: Mod CommandFields a -> (Maybe String, [(String, ParserInfo a)])
